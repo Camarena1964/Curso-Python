9bbf6b877a56f6da8c503c5b93bddcfd41ef7ffd
Camarena <uidg6629@contiwan.com>
2021-09-16 20:14:06 -0500

    mi primer programa

---
+++ Hola_mundo.py
@@ -0,0 +1,24 @@
+
+#message1 = "hola mundo"
+#message2 = "Adios mundo"
+
+#print(message1)
+#print(message2)
+
+#print(message2.upper())
+#print(message2.lower())
+
+nombre="Sandra"
+apellido="Camarena"
+
+print(nombre + " " + apellido)
+
+nombre_completo_f_string=f"{nombre} {apellido}"
+
+print(nombre_completo_f_string)
+
+texto = "uno\ndos\ttres"
+print(texto)
+
+mi_mensaje= "     hola      "
+print(mi_mensaje.strip())
